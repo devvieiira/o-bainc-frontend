9e02cbe79e62e8413982bd327cc92819ff756421
Rafael Vargas <rafael.vargas.emp@gmail.com>
2025-07-01 20:59:51 -0300

    feat: add menu options

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,91 @@
 import { Button } from "@/components/ui/button";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
+	const navLinks = [
+		{ href: "#", text: "Soluções" },
+		{ href: "#", text: "Produtos" },
+		{ href: "#", text: "Corporativo" },
+		{ href: "#", text: "Sobre" },
+		{ href: "#", text: "Contato" },
+	];
 	return (
 		<main className="flex min-h-screen w-full flex-col items-center ">
-			<header className="w-full z-10 fixed flex items-center justify-between px-5 right-0 left-0 top-5">
-				<div className="glass flex items-center justify-center py-5 rounded-2xl w-52 h-14">
+			<header className="w-full z-10 fixed flex items-center justify-between px-5 right-0 left-0 top-10">
+				<div className="glass flex items-center justify-center py-5 rounded-2xl w-52 h-16">
 					<Image
-						className="flex items-center justify-center"
+						className="w-full h-auto p-4"
 						src={"/logo.svg"}
 						alt="o-bainc logo"
 						width={170}
 						height={55}
 					/>
 				</div>
-				<div className="flex h-12 w-12 lg:w-fit items-center justify-center rounded-2xl border-white/20 bg-white/10 p-2 glass">
+				<div className="flex md:h-16 md:w-fit h-12 w-12 lg:w-fit items-center justify-center rounded-2xl border-white/20 bg-white/10 p-2 glass">
 					<ul className="flex h-full w-full items-center justify-center gap-x-8 font-bold px-4">
-						<li className="hidden lg:block">
-							<a href="#" className="transition-colors hover:text-white/70">
-								Soluções
-							</a>
-						</li>
-						<li className="hidden lg:block">
-							<a href="#" className="transition-colors hover:text-white/70">
-								Produtos
-							</a>
-						</li>
-						<li className="hidden lg:block">
-							<a href="#" className="transition-colors hover:text-white/70">
-								Corporativo
-							</a>
-						</li>
-						<li className="hidden lg:block">
-							<a href="#" className="transition-colors hover:text-white/70">
-								Sobre
-							</a>
-						</li>
-						<li className="hidden lg:block">
-							<a href="#" className="transition-colors hover:text-white/70">
-								Contato
-							</a>
-						</li>
-
-						<li className="flex items-center justify-center text-center ">
-							<Menu className="h-6 w-6 text-white" />
+						{navLinks.map((link) => (
+							<li
+								key={link.text}
+								className="hidden lg:block rounded-xl hover:text-black hover:bg-white  transition-all px-3 py-2"
+							>
+								<a href={link.href} className="">
+									{link.text}
+								</a>
+							</li>
+						))}
+						<li className="flex items-center justify-center text-center p-1 rounded-xl hover:text-black hover:bg-white  transition-all">
+							<Popover>
+								<PopoverTrigger asChild>
+									<Menu className="h-6 w-6" />
+								</PopoverTrigger>
+								<PopoverContent
+									align="end"
+									sideOffset={12}
+									alignOffset={-28}
+									className="w-fit "
+								>
+									<ul className="flex flex-col gap-2 p-2 text-center font-bold">
+										{navLinks.map((link) => (
+											<li
+												key={link.text}
+												className="py-2 md:hidden rounded-xl hover:text-black hover:bg-white transition-all"
+											>
+												<a href={link.href} className="w-full p-3 text-center">
+													{link.text}
+												</a>
+											</li>
+										))}
+										<li className="py-2 rounded-xl hover:text-black hover:bg-white transition-all">
+											<a href={"#"} className="w-full p-3 text-center">
+												outras opções
+											</a>
+										</li>
+										<li className="py-2 rounded-xl hover:text-black hover:bg-white transition-all">
+											<a href={"#"} className="w-full p-3 text-center">
+												outras opções
+											</a>
+										</li>
+										<li className="py-2 rounded-xl hover:text-black hover:bg-white transition-all">
+											<a href={"#"} className="w-full p-3 text-center">
+												outras opções
+											</a>
+										</li>
+									</ul>
+								</PopoverContent>
+							</Popover>
 						</li>
 					</ul>
 				</div>
 			</header>
 
 			<div className="w-full m-4 px-4">
-				<section className="relative w-full h-[95vh] flex flex-col items-center justify-between text-center rounded-4xl bg-gradient-to-br from-[#080B36] via-[#101569B2] to-[#ECF8F7]">
+				<section className="relative w-full h-[95vh] flex flex-col items-center justify-between text-center rounded-4xl bg-gradient-to-br from-[#080B36] via-60% via-[#101569B2] to-slate-500">
 					<div className="md:hidden w-full h-auto">
 						<Image
 							src={"/header-mobile.png"}
